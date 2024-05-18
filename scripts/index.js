99314dd267680d46e02be7adceaef0cd2dd8bdb8
Cauê Farias Santos <caue.fsantos97@gmail.com>
2024-05-18 17:08:49 -0300

    feat: Adicionar detalhamento de equipamentos e status
    Adiciona detalhamento de equipamentos e status no HTML, exibindo informações sobre sensores e laser de detecção de eixos. Também adiciona a exibição do último incidente registrado. Além disso, atualiza o JavaScript para adicionar funcionalidade de alterar o status dos equipamentos ao clicar nos botões. Por fim, adiciona estilos para os botões de status ativos, melhorando a usabilidade e a clareza da interface do usuário.

--- scripts/index.js
+++ scripts/index.js
@@ -1,9 +1,38 @@
+const setFailedEquip = e => {
+  e.classList.remove('status-ok');
+
+  e.innerHTML = 'Falha';
+  e.classList.add('status-fail');
+}
+
+const setOkEquip = e => {
+  e.classList.remove('status-fail');
+
+  e.innerHTML = 'OK';
+  e.classList.add('status-ok');
+}
+
 const activateButton = (e) => {
   const activeButton = document.querySelector('.active-button');
   const monitorTitle = document.getElementById('monitor-title');
+  const detail = document.getElementById('detalhamento');
 
   if (activeButton) {
     activeButton.classList.remove('active-button');
+  }
+
+  if (detail.hidden) {
+    detail.hidden = false;
+  }
+
+  if(e.classList.contains('status-fail')) {
+    const failedEquip = document.getElementById('sensor-5_6');
+    setFailedEquip(failedEquip);
+  }
+
+  if (e.classList.contains('status-ok')) {
+    const okEquip = document.getElementById('sensor-5_6');
+    setOkEquip(okEquip);
   }
 
   e.classList.add('active-button');
